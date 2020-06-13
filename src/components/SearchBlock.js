9cc37bb490e1b1db1bac6a5c64f6c732a6da3066
bazhena-matyuk <bazhena.m@gmail.com>
2020-06-13 14:57:12 +0300

    less1 start refactor

---
+++ src/components/SearchBlock.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBlock = () => {
+    return <input placeholder="search" />
+  }
+
+export default SearchBlock;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBlock from './components/SearchBlock'
 
-const el = <h1>Реакт элемент</h1>;
+const Header = () => {
+  return <h1>My Todo List</h1>
+}
 
-ReactDOM.render(el, document.getElementById('root'));
+
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn html</li>
+      <li>Learn css</li>
+      <li>Learn react</li>
+    </ul>
+  );
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <SearchBlock />
+      <TodoList />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
